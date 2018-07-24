f4593ae67cf9dfc780f8f00b1113acc330635e05
Chris Brown <cbrown@claritydesign.com>
2018-07-24 12:40:45 -0700

    Rebuilt project, with modified name to indicate board 02.

--- project_zero_app_cc2650launchxl/Application/project_zero.c
+++ project_zero_app_cc2650launchxl/Application/project_zero.c
@@ -203,12 +203,12 @@ static uint8_t advertData[] =
   // complete name
   13,
   GAP_ADTYPE_LOCAL_NAME_COMPLETE,
-  'P', 'r', 'o', 'j', 'e', 'c', 't', ' ', 'Z', 'e', 'r', 'o',
+  'A', 'm', 'p', ' ', 'J', 'u', 'l', '1', '8', '_', '0', '2',
 
 };
 
 // GAP GATT Attributes
-static uint8_t attDeviceName[GAP_DEVICE_NAME_LEN] = "Project Zero";
+static uint8_t attDeviceName[GAP_DEVICE_NAME_LEN] = "Amp Jul18_02";
 
 // Globals used for ATT Response retransmission
 static gattMsgEvent_t *pAttRsp = NULL;
